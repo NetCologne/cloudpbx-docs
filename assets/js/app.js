e3d15e87840a2f39ddb945924f8c1d6232e87635
Eugen <negbie@posteo.de>
2022-03-15 11:27:47 +0100

    Format

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,13 @@
 
 if (document.querySelector('#sidebar-default') !== null) {
     let sidebar = document.getElementById('sidebar-default');
-  
+
     let pos = sessionStorage.getItem('sidebar-scroll');
     if (pos !== null) {
         sidebar.scrollTop = parseInt(pos, 10);
     }
-  
+
     window.addEventListener('beforeunload', () => {
         sessionStorage.setItem('sidebar-scroll', sidebar.scrollTop);
     });
-  }
+}
